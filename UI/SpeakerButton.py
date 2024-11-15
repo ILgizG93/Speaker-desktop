bf4ebbf5b6bb8bd979dc4e7ad60a0a0f6920cfd6
ADMINion <ilgiz_g93@mail.ru>
2024-11-15 10:52:33 +0300

    change font size

--- UI/SpeakerButton.py
+++ UI/SpeakerButton.py
@@ -2,10 +2,10 @@ from PySide6.QtWidgets import QPushButton
 from PySide6.QtCore import Qt
 
 class SpeakerButton(QPushButton):
-    def __init__(self, text, width: int = 160, height: int = 38):
+    def __init__(self, text, width: int = 160, height: int = 42):
         super().__init__()
         self.setFixedWidth(width)
         self.setFixedHeight(height)
-        self.setStyleSheet('border:1px solid silver; padding:12px;')
+        self.setStyleSheet('border:1px solid silver; padding:6px; font-weight:600; font-size:14px;')
         self.setText(text)
         self.setCursor(Qt.CursorShape.PointingHandCursor)
